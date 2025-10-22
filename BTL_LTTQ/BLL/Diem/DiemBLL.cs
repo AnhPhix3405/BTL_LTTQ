9201d4ede9b5dc9082d0f6bd257502793eed953c
haidk24 <vuthanhhai24112004@gmail.com>
2025-10-22 22:00:30 +0700

    add DiemBLL,DiemDAL,DTO.Score

---
+++ BTL_LTTQ/BLL/Diem/DiemBLL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTL_LTTQ.DAL.Diem;
+using BTL_LTTQ.DTO;
+
+namespace BTL_LTTQ.BLL.Diem
+{
+    internal class DiemBLL
+    {
+        private DiemDAL dal = new DiemDAL();
+
+        public DataTable LayDanhSach()
+        {
+            return dal.GetAll();
+        }
+
+        public string ThemDiem(Score diem)
+        {
+            if (string.IsNullOrWhiteSpace(diem.MaSV) ||
+                string.IsNullOrWhiteSpace(diem.TenSV) ||
+                string.IsNullOrWhiteSpace(diem.MaLop))
+                return "Không được để trống.";
+
+            // kiểm tra giá trị điểm hợp lệ
+            if (diem.DiemCc < 0 || diem.DiemGK < 0 || diem.DiemThi < 0)
+                return "Điểm không được âm.";
+
+            if (dal.CheckExist(diem.MaSV))
+                return "Sinh viên này đã có điểm!";
+
+            return dal.Insert(diem) ? "Thêm thành công!" : "Thêm thất bại!";
+        }
+
+        public string SuaDiem(Score diem)
+        {
+            if (Double.IsNullOrWhiteSpace(diem.DiemCc))
+                return "Điểm chuyên cần không được để trống.";
+            if (Double.IsNullOrWhiteSpace(diem.DiemGK))
+                return "Điểm giữa kì không được để trống.";
+            if (Double.IsNullOrWhiteSpace(diem.DiemThi))
+                return "Điểm Thi không được để trống.";
+
+            return dal.Update(diem) ? "Sửa thành công!" : "Sửa thất bại!";
+        }
+
+        public string XoaSanPham(string MaSV)
+        {
+            return dal.Delete(MaSV) ? "Xóa thành công!" : "Xóa thất bại!";
+        }
+    }
+}
---
+++ BTL_LTTQ/DAL/Diem/DiemDAL.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_LTTQ.DAL.Diem
+{
+    internal class DiemDAL
+    {
+        private string connectionString = "Data Source=LAPTOP-LHTIMU8S;Initial Catalog=QuanLySanPham;Integrated Security=True;";
+        private SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+        public DataTable GetAll()
+        {
+            using (SqlConnection conn=GetConnection())
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Diem", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+        public bool Insert(Diem diem)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                string query = "INSERT INTO Diem (MaLop, MaSV,DiemCc,DiemGK,DiemThi) VALUES (@MaLop, @MaSV,@DiemCc,@DiemGK,@DiemThi)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaLop", diem.MaLop);
+                cmd.Parameters.AddWithValue("@MaSV", diem.MaSV);
+                cmd.Parameters.AddWithValue("@DiemCc", diem.DiemCc);
+                cmd.Parameters.AddWithValue("@DiemGK", diem.DiemGK);
+                cmd.Parameters.AddWithValue("@DiemThi", diem.DiemThi);              
+                conn.Open();
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+        public bool Update(Diem diem)//chua fix
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                string query = "UPDATE SanPham SET DiemCc = @DiemCc, DiemGK=@DiemGK, DiemThi=@DiemThi WHERE MaSV = @MaSV";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@DiemCc", diem.DiemCc);
+                cmd.Parameters.AddWithValue("@DiemGK", diem.DiemGK);
+                cmd.Parameters.AddWithValue("@DiemThi", diem.DiemThi);
+                conn.Open();
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+        public bool Delete(string MaSV)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                string query = "DELETE FROM Diem WHERE MaSV = @MaSV";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaSV", MaSV);
+                conn.Open();
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+        public bool CheckExist(string MaSV)
+        {
+            using (SqlConnection conn = GetConnection())
+            {
+                string query = "SELECT COUNT(*) FROM Diem WHERE MaSV = @MaSV";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@MaSV", MaSV);
+                conn.Open();
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+
+    }
+}
